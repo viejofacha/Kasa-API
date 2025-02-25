c71e830916e4d372c260a2d09d5bc853ee4cbaa9
MaçonWeb <aleandro67@gmail.com>
2025-02-25 13:26:12 +0100

    feat: Structuration initiale du projet avec composants, pages et mise en forme

---
+++ Kasa API/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import "./home.css"
+
+export default function Home() {
+
+    // gérer la logique pour faire un appel api (fetch) pour récupérer les données
+    
+    // gérer la logique pour afficher les données
+  return (
+    <div className="home-container">Home</div>
+  )
+}
