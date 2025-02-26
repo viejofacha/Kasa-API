0e5547a78e47f3928247fdb0fe6ed0c453e92223
MaçonWeb <aleandro67@gmail.com>
2025-02-26 20:03:26 +0100

    feat: Organisation de la structure du SRC - Création de dossiers et de fichiers -Composants

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header/Header'
+import Header from './layout/header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
