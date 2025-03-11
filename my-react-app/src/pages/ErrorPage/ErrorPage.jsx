2619b70b4e9667b74ce0e212283aced2895551f0
MaçonWeb <aleandro67@gmail.com>
2025-03-11 10:56:09 +0100

    SMART : Mettre en forme la page 404.

--- my-react-app/src/pages/ErrorPage/ErrorPage.jsx
+++ my-react-app/src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,13 @@
-
-
+import ImageError from "../../assets/404.svg"
+import "../../pages/ErrorPage/errorPage.css"
 export default function ErrorPage() {
   return (
-    <div>ErrorPage</div>
+    <div className= "ErrorPage">
+      <ul>
+         <img src={ImageError} alt="Page Error" className="img404" />
+        <h1 className="ErrorPage-h1">Oups! La page que vous demandez n`existe pas.</h1>
+        <div className="ErrorPage-a"><a href={'http://localhost:5173/'}>Retourner sur la page d’accueil</a></div>
+      </ul>
+    </div>
   )
 }
